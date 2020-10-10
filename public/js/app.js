572bc41b45fde5e543170792d00bb63cb134a908
pradeeptn <pradeep.tadoor@tribe3.com>
2020-10-10 13:40:11 +0530

    removed unnecessary console.log

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,4 @@
 
-console.log("client side javascript file is loaded");
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
