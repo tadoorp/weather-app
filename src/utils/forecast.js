b8a048edee44c7bd6c48776172370245e24e44f2
pradeeptn <pradeep.tadoor@tribe3.com>
2020-10-10 18:59:24 +0530

    added humidity information

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -13,7 +13,7 @@ const forecast = (lattitude, longitude, callback) => {
         }
         else {
             let current = body.current
-            callback(undefined, `${current.weather_descriptions}. It is currently ${current.temperature} degrees out. It feels like ${current.feelslike} degrees out`)
+            callback(undefined, `${current.weather_descriptions}. It is currently ${current.temperature} degrees out. It feels like ${current.feelslike} degrees out. The humidity is ${current.humidity}.`)
         }
     })
 }
